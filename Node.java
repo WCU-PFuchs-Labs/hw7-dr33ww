cb8c1a97f7e8271230d5142387a5fb225cb36365
dr33ww <ag950405@wcupa.edu>
2025-10-30 00:23:50 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,45 +1,84 @@
 
 
+import java.util.Random;
 
 public class Node {
-    Op op;
+    Op operation;
     Node lChild;
     Node rChild;
 
     
     public Node(Op o) {
-        op = o;
-        lChild = null;
-        rChild = null;
+        this.operation = o;
+        this.lChild = null;
+        this.rChild = null;
     }
 
-    // add random kids using the factory
-    public void addRandomKids(NodeFactory n, int maxDepth, java.util.Random rand) {
-        op.addRandomKids(this, n, maxDepth, rand);
+    // build children maxdepth
+    
+    public void addRandomKids(NodeFactory factory, int maxDepth, Random rand) {
+        if (maxDepth <= 0) {
+            return; // stop growing
+        }
+
+        if (operation instanceof Binop) {
+            // binop needs two children
+            lChild = new Node(factory.getOperator(rand));
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
+
+            rChild = new Node(factory.getOperator(rand));
+            rChild.addRandomKids(factory, maxDepth - 1, rand);
+
+        } else if (operation instanceof Unop) {
+            // unop needs one child 
+            lChild = new Node(factory.getOperator(rand));
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
+            // right remains null
+
+        } else {
+            // terminal stay leaves
+        }
     }
 
-    // eval node given data
+    // evaluate this node on the given data
     public double eval(double[] data) {
-       
-        return op.eval(this, data);
+        if (operation instanceof Binop) {
+            double a = lChild.eval(data);
+            double b = rChild.eval(data);
+            return ((Binop) operation).eval(a, b);
+        } else if (operation instanceof Unop) {
+            double a = lChild.eval(data);
+            return ((Unop) operation).eval(a);
+        } else {
+            
+            return operation.eval(data);
+        }
     }
 
-    // turn node into readable str
+    // conver node readable str
     public String toString() {
-        
-        return op.toString(this);
+        if (operation instanceof Binop) {
+            
+            return "(" + lChild.toString() + " " + operation.toString() + " " + rChild.toString() + ")";
+        } else if (operation instanceof Unop) {
+            
+            return operation.toString() + "(" + lChild.toString() + ")";
+        } else {
+            
+            return operation.toString();
+        }
     }
 
 
 
-   
+    // preorder traversal
     public void traverse(Collector c) {
         c.collect(this);
         if (this.lChild != null) this.lChild.traverse(c);
         if (this.rChild != null) this.rChild.traverse(c);
     }
 
-    // true if node has no children, who is gohan
+    // check: if no children
     public boolean isLeaf() {
         return this.lChild == null && this.rChild == null;
     }
@@ -51,12 +90,13 @@ public class Node {
         trunk.lChild = temp;
     }
 
-    // swap this node's right child with trunk's right child, gohannnn
+    // swap this node's right child with trunk's right child, gohannn
     public void swapRight(Node trunk) {
         Node temp = this.rChild;
         this.rChild = trunk.rChild;
         trunk.rChild = temp;
     }
 
-    
+  
 }
+
