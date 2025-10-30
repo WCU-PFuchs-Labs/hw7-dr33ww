909d3660dcc7a5a4bca2b8c835df35ffe47efd48
dr33ww <ag950405@wcupa.edu>
2025-10-30 07:29:28 -0400

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -1,17 +1,13 @@
 public class Divide extends Binop {
-    public Divide() {}
-    public Divide(Node l, Node r) {
-        super(l, r);
-    }
-    public double eval(double[] data) {
-        double divisor = rChild.eval(data);
-   
-        if (Math.abs(divisor) < 0.0001) {
+    public double eval(double left, double right) {
+     
+        if (Math.abs(right) < 0.0001) {
             return 1.0;
         }
-        return lChild.eval(data) / divisor;
+        return left / right;
     }
+    
     public String toString() {
-        return "(" + lChild.toString() + " / " + rChild.toString() + ")";
+        return "/";
     }
 }
