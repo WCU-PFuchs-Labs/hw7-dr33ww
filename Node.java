2c58c1cbe6b8b84e8f376afc2a0aac667b7323c6
dr33ww <ag950405@wcupa.edu>
2025-10-30 02:24:41 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,121 +1,87 @@
 import java.util.Random;
 
-package binary;
-
-import java.util.ArrayList;
-import java.util.Random;
-
 public class Node {
 
     Op operation;
     Node lChild;
     Node rChild;
 
-    // empty constructor
     public Node() {
-        operation = null;
-        lChild = null;
-        rChild = null;
+        this.operation = null;
+        this.lChild = null;
+        this.rChild = null;
     }
 
-    // create node with an operation
-    public Node(Op o) {
-        operation = o;
-        lChild = null;
-        rChild = null;
+    public Node(Op operation) {
+        this.operation = operation;
+        this.lChild = null;
+        this.rChild = null;
     }
 
-    // set left child
-    public void setLeft(Node n) {
-        lChild = n;
+    public void setLeft(Node n) { this.lChild = n; }
+    public void setRight(Node n) { this.rChild = n; }
+    public Node getLeft() { return lChild; }
+    public Node getRight() { return rChild; }
+    public Op getOperation() { return operation; }
+    public void setOperation(Op o) { this.operation = o; }
+
+    // check if this node is a leaf
+    public boolean isLeaf() {
+        return lChild == null && rChild == null;
     }
 
-    // set right child
-    public void setRight(Node n) {
-        rChild = n;
+    // swap this node's left child with another node's left child
+    public void swapLeft(Node other) {
+        Node temp = this.lChild;
+        this.lChild = other.lChild;
+        other.lChild = temp;
     }
 
-    // return left child
-    public Node getLeft() {
-        return lChild;
+    // swap this node's right child with another node's right child
+    public void swapRight(Node other) {
+        Node temp = this.rChild;
+        this.rChild = other.rChild;
+        other.rChild = temp;
     }
 
-    // return right child
-    public Node getRight() {
-        return rChild;
-    }
-
-    // get operation
-    public Op getOperation() {
-        return operation;
-    }
-
-    // set operation
-    public void setOperation(Op o) {
-        operation = o;
-    }
-
-    // evaluate recursively
-    public double eval(double[] data) {
-        if (operation instanceof Const) {
-            return ((Const) operation).getVal();
-        } 
-        else if (operation instanceof Variable) {
-            return ((Variable) operation).eval(data);
-        } 
-        else if (operation instanceof Unop) {
-            return ((Unop) operation).eval(lChild.eval(data));
-        } 
-        else if (operation instanceof Binop) {
-            double leftVal = lChild.eval(data);
-            double rightVal = rChild.eval(data);
-            return ((Binop) operation).eval(leftVal, rightVal);
-        } 
-        else {
-            return 0.0;
-        }
-    }
-
-    // print the expression
-    public String toString() {
-        if (operation == null) {
-            return "?";
-        }
-        if (operation instanceof Const || operation instanceof Variable) {
-            return operation.toString();
-        } 
-        else if (operation instanceof Unop) {
-            return operation.toString() + "(" + lChild.toString() + ")";
-        } 
-        else if (operation instanceof Binop) {
-            return "(" + lChild.toString() + " " + operation.toString() + " " + rChild.toString() + ")";
-        }
-        return "?";
-    }
-
-    // traverse and collect binops
+    // visit all nodes and collect only binops
     public void traverse(Collector c) {
         if (operation instanceof Binop) {
             c.collect(this);
         }
-        if (lChild != null) {
-            lChild.traverse(c);
-        }
-        if (rChild != null) {
-            rChild.traverse(c);
+        if (lChild != null) lChild.traverse(c);
+        if (rChild != null) rChild.traverse(c);
+    }
+
+    // evaluate the node recursively
+    public double eval(double[] data) {
+        if (operation == null) return 0.0;
+
+        if (operation instanceof Const) {
+            return ((Const) operation).eval(data);
+        } else if (operation instanceof Variable) {
+            return ((Variable) operation).eval(data);
+        } else if (operation instanceof Unop) {
+            return ((Unop) operation).eval(lChild.eval(data));
+        } else if (operation instanceof Binop) {
+            double leftVal = lChild.eval(data);
+            double rightVal = rChild.eval(data);
+            return ((Binop) operation).eval(leftVal, rightVal);
+        } else {
+            return 0.0;
         }
     }
 
-    // used by nodefactory to add random kids
-    public void addRandomKids(NodeFactory factory, int depth, Random rand) {
-        if (depth > 0) {
-            if (operation instanceof Binop) {
-                lChild = factory.randomNode(depth - 1, rand);
-                rChild = factory.randomNode(depth - 1, rand);
-            } 
-            else if (operation instanceof Unop) {
-                lChild = factory.randomNode(depth - 1, rand);
-            }
+    // build a readable string of the tree
+    public String toString() {
+        if (operation == null) return "?";
+        if (operation instanceof Const || operation instanceof Variable) {
+            return operation.toString();
+        } else if (operation instanceof Unop) {
+            return operation.toString() + "(" + lChild.toString() + ")";
+        } else if (operation instanceof Binop) {
+            return "(" + lChild.toString() + " " + operation.toString() + " " + rChild.toString() + ")";
         }
+        return "?";
     }
 }
