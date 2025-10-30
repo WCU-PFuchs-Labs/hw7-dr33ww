b179a65877ae0d5f4acc2da5d9577bdd68e1ac80
dr33ww <ag950405@wcupa.edu>
2025-10-30 04:17:07 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,40 +5,27 @@ public class Node {
     private Node lChild;
     private Node rChild;
 
-    public Node(Op op) {
-        this.operation = op;
+    public Node(Op operation) {
+        this.operation = operation;
     }
-
-   
-    public Node(Node n) {
-        this.operation = n.operation;
-        this.lChild = n.lChild;
-        this.rChild = n.rChild;
-    }
-
-   
-    public Op getOperation() { return operation; }
-    public Node getLeft()     { return lChild;    }
-    public Node getRight()    { return rChild;    }
-    public void setLeft(Node n)  { this.lChild = n; }
-    public void setRight(Node n) { this.rChild = n; }
 
     
+    public Node getLeft() { return lChild; }
+    public Node getRight() { return rChild; }
+    public Op getOperation() { return operation; }
+
+
     public double eval(double[] data) {
         if (operation instanceof Binop) {
-            double a = (lChild != null) ? lChild.eval(data) : 0.0;
-            double b = (rChild != null) ? rChild.eval(data) : 0.0;
-            return ((Binop) operation).eval(new double[]{ a, b });
+            double a = lChild.eval(data);
+            double b = rChild.eval(data);
+            return ((Binop) operation).eval(a, b); 
         } else if (operation instanceof Unop) {
-            double v = (lChild != null) ? lChild.eval(data) : 0.0;
-            return ((Unop) operation).eval(new double[]{ v });
-        } else if (operation instanceof Const) {
-            return ((Const) operation).eval(new double[0]);
-        } else if (operation instanceof Variable) {
-            return ((Variable) operation).eval(data);
+            double v = lChild.eval(data);
+            return ((Unop) operation).eval(v); 
+        } else {
+            return operation.eval(data); 
         }
-       
-        return 0.0;
     }
 
    
@@ -48,45 +35,12 @@ public class Node {
         if (operation instanceof Binop) {
             if (lChild == null) lChild = factory.getOperator(rand);
             if (rChild == null) rChild = factory.getOperator(rand);
-            if (lChild != null) lChild.addRandomKids(factory, maxDepth - 1, rand);
-            if (rChild != null) rChild.addRandomKids(factory, maxDepth - 1, rand);
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
+            rChild.addRandomKids(factory, maxDepth - 1, rand);
         } else if (operation instanceof Unop) {
             if (lChild == null) lChild = factory.getOperator(rand);
-            if (lChild != null) lChild.addRandomKids(factory, maxDepth - 1, rand);
-        } else {
-          
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
         }
-    }
-
- 
-    public void collect(Collector c) {
-        if (operation instanceof Binop) {
-            c.collect(this);
-        }
-        if (lChild != null) lChild.collect(c);
-        if (rChild != null) rChild.collect(c);
-    }
-
- 
-    public String toString() {
-        if (operation instanceof Binop) {
-            String leftS  = (lChild != null ? lChild.toString() : "null");
-            String rightS = (rChild != null ? rChild.toString() : "null");
-            return ((Binop) operation).toString(leftS, rightS);
-        } else if (operation instanceof Unop) {
-            String arg = (lChild != null ? lChild.toString() : "null");
-            
-            return operation.toString() + "(" + arg + ")";
-        } else {
-            return operation.toString();
-        }
-    }
-
-    public Node deepCopy() {
-        Node n = new Node(operation);
-        if (lChild != null) n.lChild = lChild.deepCopy();
-        if (rChild != null) n.rChild = rChild.deepCopy();
-        return n;
     }
 
     
@@ -103,5 +57,26 @@ public class Node {
         other.lChild = tmpL;
         other.rChild = tmpR;
     }
-}
 
+   
+    public void collect(Collector c) {
+        if (operation instanceof Binop) {
+            c.collect(this);
+        }
+        if (lChild != null) lChild.collect(c);
+        if (rChild != null) rChild.collect(c);
+    }
+
+    
+    public String toString() {
+        if (operation instanceof Binop) {
+            String leftS = (lChild == null) ? "?" : lChild.toString();
+            String rightS = (rChild == null) ? "?" : rChild.toString();
+            return "(" + leftS + " " + operation.toString() + " " + rightS + ")";
+        } else if (operation instanceof Unop) {
+            return operation.toString() + "(" + (lChild == null ? "?" : lChild.toString()) + ")";
+        } else {
+            return operation.toString();
+        }
+    }
+}
