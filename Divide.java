c4ff138fd059bdddee5c71445f34dbfc9084fa91
dr33ww <ag950405@wcupa.edu>
2025-10-29 23:00:35 -0400

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -1,7 +1,21 @@
-
 public class Divide extends Binop {
-    public double eval(double left, double right) {
-        return left / right;
+
+    // gohan gohannn
+    public Divide() { }
+
+    // returns the string form for printing, what did they do to u gohan 
+    public String toString(Node n) {
+        return "(" + n.lChild.toString() + " / " + n.rChild.toString() + ")";
     }
-    public String toString() { return "/"; }
+
+    // safe evaluation to avoid divide by zero
+    public double eval(Node n, double[] data) {
+        double denom = n.rChild.eval(data);   // evaluate right child
+        if (Math.abs(denom) < 0.0001) {
+            
+            return 1.0;
+        }
+        double numer = n.lChild.eval(data);   // evaluate left child, this is for gohannn
+        return numer / denom;
+    }
 }
