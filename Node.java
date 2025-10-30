eaf67a1b97f6d5e2df132119177bf325e7083153
dr33ww <ag950405@wcupa.edu>
2025-10-30 03:31:24 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -15,56 +15,59 @@ public class Node {
 
     public double eval(double[] data) {
         if (operation instanceof Binop) {
-            if (lChild == null || rChild == null) {
+            if (lChild == null || rChild == null)
                 throw new NullPointerException("Binop node missing child(ren)");
-            }
             double a = lChild.eval(data);
             double b = rChild.eval(data);
             return ((Binop) operation).eval(a, b);
-        } else if (operation instanceof Unop) {
-            
+        } 
+        else if (operation instanceof Unop) {
             return ((Unop) operation).eval(data);
-        } else if (operation instanceof Variable) {
+        } 
+        else if (operation instanceof Variable) {
             return ((Variable) operation).eval(data);
-        } else if (operation instanceof Const) {
+        } 
+        else if (operation instanceof Const) {
             return ((Const) operation).eval();
-        } else {
+        } 
+        else {
             return 0.0;
         }
     }
 
-    public void collect(Collector c) {
+    public void addRandomKids(NodeFactory factory, int maxDepth, Random rand) {
+        if (maxDepth <= 0) {
+            lChild = null;
+            rChild = null;
+            return;
+        }
+
         if (operation instanceof Binop) {
-            String leftS = (lChild != null) ? lChild.toString() : "null";
-            String rightS = (rChild != null) ? rChild.toString() : "null";
-            String expr = "(" + leftS + " " + operation.toString() + " " + rightS + ")";
-            c.collect(expr);
-            if (lChild != null) lChild.collect(c);
-            if (rChild != null) rChild.collect(c);
-        } else {
-            if (lChild != null) lChild.collect(c);
-            if (rChild != null) rChild.collect(c);
+            lChild = factory.getOperator(rand);
+            rChild = factory.getOperator(rand);
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
+            rChild.addRandomKids(factory, maxDepth - 1, rand);
+        } 
+        else if (operation instanceof Unop) {
+            lChild = factory.getOperator(rand);
+            lChild.addRandomKids(factory, maxDepth - 1, rand);
+        }
+        else {
+            lChild = null;
+            rChild = null;
         }
     }
 
-    public String toString() {
+    public void traverse(Collector c) {
         if (operation instanceof Binop) {
             String leftS = (lChild != null) ? lChild.toString() : "null";
             String rightS = (rChild != null) ? rChild.toString() : "null";
-            return "(" + leftS + " " + operation.toString() + " " + rightS + ")";
-        } else if (operation instanceof Unop) {
-            String inner = (lChild != null) ? lChild.toString() : "?";
-            return operation.toString() + "(" + inner + ")";
-        } else if (operation instanceof Variable) {
-            return operation.toString();
-        } else if (operation instanceof Const) {
-            return operation.toString();
-        } else {
-            return operation.toString();
+            c.collect("(" + leftS + " " + operation.toString() + " " + rightS + ")");
         }
+        if (lChild != null) lChild.traverse(c);
+        if (rChild != null) rChild.traverse(c);
     }
 
-    
     public void swapWith(Node other) {
         Op tmpOp = this.operation;
         Node tmpL = this.lChild;
@@ -77,5 +80,23 @@ public class Node {
         other.operation = tmpOp;
         other.lChild = tmpL;
         other.rChild = tmpR;
+    }
+
+    public String toString() {
+        if (operation instanceof Binop) {
+            String leftS = (lChild != null) ? lChild.toString() : "null";
+            String rightS = (rChild != null) ? rChild.toString() : "null";
+            return "(" + leftS + " " + operation.toString() + " " + rightS + ")";
+        } 
+        else if (operation instanceof Unop) {
+            String inner = (lChild != null) ? lChild.toString() : "?";
+            return operation.toString() + "(" + inner + ")";
+        } 
+        else if (operation instanceof Variable || operation instanceof Const) {
+            return operation.toString();
+        } 
+        else {
+            return operation.toString();
+        }
     }
 }
