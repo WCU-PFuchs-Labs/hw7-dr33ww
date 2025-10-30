42a91e6b24f65119baef36d97b3dc4d7a75af97f
dr33ww <ag950405@wcupa.edu>
2025-10-30 02:21:37 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,106 +1,121 @@
 import java.util.Random;
 
+package binary;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Node {
+
     Op operation;
     Node lChild;
     Node rChild;
 
+    // empty constructor
+    public Node() {
+        operation = null;
+        lChild = null;
+        rChild = null;
+    }
+
+    // create node with an operation
     public Node(Op o) {
-        this.operation = o;
-        this.lChild = null;
-        this.rChild = null;
+        operation = o;
+        lChild = null;
+        rChild = null;
     }
 
-    // grow children up to maxDepth using a NodeFactory
-    public void addRandomKids(NodeFactory factory, int maxDepth, Random rand) {
-        if (maxDepth <= 0) return;
-
-        if (operation instanceof Binop) {
-            // two children
-            lChild = factory.getOperator(rand);
-            if (lChild != null) lChild.addRandomKids(factory, maxDepth - 1, rand);
-
-            rChild = factory.getOperator(rand);
-            if (rChild != null) rChild.addRandomKids(factory, maxDepth - 1, rand);
-
-        } else if (operation instanceof Unop) {
-            // one child only
-            lChild = factory.getOperator(rand);
-            if (lChild != null) lChild.addRandomKids(factory, maxDepth - 1, rand);
-        } else {
-            // no children (const or var)
-        }
+    // set left child
+    public void setLeft(Node n) {
+        lChild = n;
     }
 
-    // evaluate this node on the given data
+    // set right child
+    public void setRight(Node n) {
+        rChild = n;
+    }
+
+    // return left child
+    public Node getLeft() {
+        return lChild;
+    }
+
+    // return right child
+    public Node getRight() {
+        return rChild;
+    }
+
+    // get operation
+    public Op getOperation() {
+        return operation;
+    }
+
+    // set operation
+    public void setOperation(Op o) {
+        operation = o;
+    }
+
+    // evaluate recursively
     public double eval(double[] data) {
-        if (operation instanceof Binop) {
-            double a = (lChild != null) ? lChild.eval(data) : 0.0;
-            double b = (rChild != null) ? rChild.eval(data) : 0.0;
-            return ((Binop) operation).eval(a, b);
-        } else if (operation instanceof Unop) {
-    return ((Unop) operation).eval(data);
-} else if (operation instanceof Const) {
-            return ((Const) operation).eval(data);
-        } else if (operation instanceof Variable) {
+        if (operation instanceof Const) {
+            return ((Const) operation).getVal();
+        } 
+        else if (operation instanceof Variable) {
             return ((Variable) operation).eval(data);
-        } else {
-            throw new IllegalStateException("unknown Op type: " + operation.getClass().getName());
+        } 
+        else if (operation instanceof Unop) {
+            return ((Unop) operation).eval(lChild.eval(data));
+        } 
+        else if (operation instanceof Binop) {
+            double leftVal = lChild.eval(data);
+            double rightVal = rChild.eval(data);
+            return ((Binop) operation).eval(leftVal, rightVal);
+        } 
+        else {
+            return 0.0;
         }
     }
 
-    // convert node to readable str
+    // print the expression
     public String toString() {
-        if (operation instanceof Binop) {
-            String leftStr = (lChild != null) ? lChild.toString() : "?";
-            String rightStr = (rChild != null) ? rChild.toString() : "?";
-            return "(" + leftStr + " " + operation.toString() + " " + rightStr + ")";
-        } else if (operation instanceof Unop) {
-            String leftStr = (lChild != null) ? lChild.toString() : "?";
-            return operation.toString() + "(" + leftStr + ")";
-        } else {
+        if (operation == null) {
+            return "?";
+        }
+        if (operation instanceof Const || operation instanceof Variable) {
             return operation.toString();
+        } 
+        else if (operation instanceof Unop) {
+            return operation.toString() + "(" + lChild.toString() + ")";
+        } 
+        else if (operation instanceof Binop) {
+            return "(" + lChild.toString() + " " + operation.toString() + " " + rChild.toString() + ")";
+        }
+        return "?";
+    }
+
+    // traverse and collect binops
+    public void traverse(Collector c) {
+        if (operation instanceof Binop) {
+            c.collect(this);
+        }
+        if (lChild != null) {
+            lChild.traverse(c);
+        }
+        if (rChild != null) {
+            rChild.traverse(c);
         }
     }
 
-    // visit self, then left, then right
- public void traverse(Collector c) {
-    // collect this node if it's a Binop
-    if (operation instanceof Binop) {
-        c.collect(this);
-    }
-
-    // always traverse left and right if they exist
-    if (lChild != null) {
-        lChild.traverse(c);
-    }
-
-    if (rChild != null) {
-        rChild.traverse(c);
-    }
-
-    // come on bruh its commmit 20 like dead ses
-    if (operation instanceof Unop && lChild != null) {
-        lChild.traverse(c);
-    }
-}
-
-    // true if no children
-    public boolean isLeaf() {
-        return this.lChild == null && this.rChild == null;
-    }
-
-    // swap this node's left child with trunk's left child, gohan
-    public void swapLeft(Node trunk) {
-        Node temp = this.lChild;
-        this.lChild = trunk.lChild;
-        trunk.lChild = temp;
-    }
-
-    // swap this node's right child with trunk's right child, what did they do to u gohan
-    public void swapRight(Node trunk) {
-        Node temp = this.rChild;
-        this.rChild = trunk.rChild;
-        trunk.rChild = temp;
+    // used by nodefactory to add random kids
+    public void addRandomKids(NodeFactory factory, int depth, Random rand) {
+        if (depth > 0) {
+            if (operation instanceof Binop) {
+                lChild = factory.randomNode(depth - 1, rand);
+                rChild = factory.randomNode(depth - 1, rand);
+            } 
+            else if (operation instanceof Unop) {
+                lChild = factory.randomNode(depth - 1, rand);
+            }
+        }
     }
 }
